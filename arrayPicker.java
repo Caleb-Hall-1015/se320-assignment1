1b7b910e27bd613d2eba2aee3ee8dafb9e7119d8
chall1015 <93002893+chall1015@users.noreply.github.com>
2022-09-15 16:41:09 -0400

    Update arrayPicker.java

--- arrayPicker.java
+++ arrayPicker.java
@@ -2,13 +2,14 @@ public class arrayPicker
 {
 	public static void main(String args[])
 	{
-		int[] randomNums = new int[100];
+		int[] randomNums = new int[100]; //100 values in the array
 		boolean flag = true;
 		java.util.Scanner scan = new java.util.Scanner(System.in);
 		
 		for (int i = 0; i < 100; i++)
 		{
-			randomNums[i] = (int) (Math.random() * 100 + 1);
+			randomNums[i] = (int) (Math.random() * 100 + 1); //multiplying by 100 to get many different values to put into array,
+			                                                 //then adding 1 to shift results from 0-99 to 1-100
 		}
 		
 		System.out.println("Enter an index to see the number at that point");
