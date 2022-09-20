fc0c115f5a74d5bb163ee28221aaa71a31aa7d3e
chall1015 <93002893+chall1015@users.noreply.github.com>
2022-09-20 15:58:15 -0400

    Update displaySum.java

--- displaySum.java
+++ displaySum.java
@@ -5,7 +5,7 @@ public class displaySum
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		int val1, val2, val3;
+		int userInput1, userInput2, sum;
 		boolean inputFlag = true;
 		
 		while (inputFlag)
@@ -13,11 +13,11 @@ public class displaySum
 			try
 			{
 				System.out.println("Please enter the first int");
-				val1 = scan.nextInt();
+				userInput1 = scan.nextInt();
 				System.out.println("Please enter the second int");
-				val2 = scan.nextInt();
-				val3 = val1 + val2;
-				System.out.format("The addition of %d and %d yields: %d", val1, val2, val3);
+				userInput2 = scan.nextInt();
+				sum = userInput1 + userInput2;
+				System.out.format("The addition of %d and %d yields: %d", userInput1, userInput2, sum);
 				inputFlag = false;
 			} catch (Exception e)
 			{
@@ -25,7 +25,6 @@ public class displaySum
 				System.out.println("Invalid input, please only use ints\n");
 			}
 		}
-		
 		scan.close();
 	}
 }
